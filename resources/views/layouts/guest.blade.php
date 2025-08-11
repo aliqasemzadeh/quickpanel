d7d3d7e85e868f91ab89244f944d651584381615
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-08-11 18:39:45 +0330

    Remove default Laravel welcome view template

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,1 +1,18 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ $title ?? "" }}</title>
+
+    @vite('resources/css/app.css')
+    @livewireStyles
+</head>
+<body>
+
+@vite('resources/js/app.js')
+@livewire('slide-over-pro')
+@livewireScripts
+</body>
+</html>
