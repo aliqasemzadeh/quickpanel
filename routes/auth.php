78dcd862336878e2fa6023dcee5c2952ccb386c4
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-08-15 06:17:23 +0330

    Remove OAuth setup guide and add register route to auth routes configuration

--- routes/auth.php
+++ routes/auth.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/login', \App\Livewire\Guest\Auth\Login::class)->name('login');
 Route::get('/logout', \App\Livewire\Guest\Auth\Logout::class)->name('logout');
+Route::get('/register', \App\Livewire\Guest\Auth\Register::class)->name('register');
 
 Route::get('/guest/auth/google/redirect', [\App\Http\Controllers\Guest\Auth\GoogleController::class, 'redirectToGoogle'])->name('auth.google.redirect');
 Route::any('/guest/auth/google/callback', [\App\Http\Controllers\Guest\Auth\GoogleController::class, 'handleGoogleCallback'])->name('auth.google.callback');
