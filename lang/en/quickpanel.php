62877ef65a883d7453881ee00bf9d2fd3cde27a0
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-08-15 06:37:14 +0330

    Refactor login view to use centralized localization keys and extend `quickpanel` translations

--- lang/en/quickpanel.php
+++ lang/en/quickpanel.php
@@ -1,4 +1,13 @@
 <?php
 return [
-    'direction' => "ltr"
+    'direction' => "ltr",
+    'login_google' => 'Sign in with Google',
+    'login_github' => 'Sign in with GitHub',
+    'or' => 'Or',
+    'remember_me' => 'Remember me',
+    'forgot_your_password' => 'Forgot your password?',
+    'register' => 'Register',
+    'log_in_to_your_account' => 'Log in to your account',
+    'email_placeholder' => 'Email address',
+    'password_placeholder' => 'Password',
 ];
