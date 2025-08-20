a72f717ad64e80ec36b0744339e1df5e57ddbc96
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-08-20 05:23:17 +0330

    Add admin system commands for roles, permissions, and admin creation; update dependencies for localization and log viewer support.

---
+++ app/Console/Commands/System/Admin/CreateAdminCommand.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands\System\Admin;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class CreateAdminCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'system:admin:create-admin-command';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $name = $this->ask('Full Name');
+        $email = $this->ask('EMail');
+        $password = $this->secret('Password');
+        try {
+            $user = User::create([
+                'name' => $name,
+                'email' => $email,
+                'password' => bcrypt($password),
+            ]);
+            $user->assignRole('user');
+            $user->assignRole('administrator');
+            $this->info('User Created Successfully');
+        } catch (\Exception $e) {
+            $this->error($e->getMessage());
+        }
+
+    }
+}
