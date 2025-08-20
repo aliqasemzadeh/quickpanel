2738ea5130b5589bff373cda2328e48c1f15b0f9
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-08-20 08:46:46 +0330

    Add `Function/Index` and `Option/Index` Livewire components and corresponding views under `Administrator\SettingManagement`

---
+++ app/Livewire/Administrator/SettingManagement/Function/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Administrator\SettingManagement\Function;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.administrator.setting-management.function.index');
+    }
+}
