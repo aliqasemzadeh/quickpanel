15cd64f4eece20fb5e21bccd66fa401994f03a0a
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-08-20 06:10:34 +0330

    Add `#[Layout]` attribute to Role and Permission components for consistent admin layout usage

--- app/Livewire/Admin/UserManagement/Permission/Index.php
+++ app/Livewire/Admin/UserManagement/Permission/Index.php
@@ -2,10 +2,12 @@
 
 namespace App\Livewire\Admin\UserManagement\Permission;
 
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class Index extends Component
 {
+    #[Layout('layouts.admin')]
     public function render()
     {
         return view('livewire.admin.user-management.permission.index');
