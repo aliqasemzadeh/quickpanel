701765f9d1ca85d8b2f34f17f17cb5f44ec457e0
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-08-23 04:38:56 +0330

    Update routes in `user.blade.php` to use `administrator` namespace instead of `admin` for consistency

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -99,7 +99,7 @@
             <ul class="space-y-2">
                 <li>
                     <a
-                        href="{{ route('admin.dashboard.index') }}"
+                        href="{{ route('administrator.dashboard.index') }}"
                         class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
                     >
                         <svg
@@ -146,21 +146,21 @@
                     <ul id="dropdown-user-management" class="py-2 space-y-2 {{ request()->routeIs('administrator.user-management.*') ? ' hidden' : ''  }}">
                         <li>
                             <a
-                                href="{{ route('admin.user-management.user.index') }}"
+                                href="{{ route('administrator.user-management.user.index') }}"
                                 class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                             >{{ __('quickpanel.users') }}</a
                             >
                         </li>
                         <li>
                             <a
-                                href="{{ route('admin.user-management.role.index') }}"
+                                href="{{ route('administrator.user-management.role.index') }}"
                                 class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                             >{{ __('quickpanel.roles') }}</a
                             >
                         </li>
                         <li>
                             <a
-                                href="{{ route('admin.user-management.permission.index') }}"
+                                href="{{ route('administrator.user-management.permission.index') }}"
                                 class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                             >{{ __('quickpanel.permissions') }}</a
                             >
