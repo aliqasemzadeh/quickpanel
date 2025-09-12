40c600658237f9ee8430faf8f9f10961d22d482a
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-09-12 10:42:01 +0330

    Use translation strings for sidebar search label and placeholder in administrator and user Blade templates.

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -73,7 +73,7 @@
     >
         <div class="overflow-y-auto py-5 px-3 h-full bg-white dark:bg-gray-800">
             <form action="#" method="GET" class="md:hidden mb-2">
-                <label for="sidebar-search" class="sr-only">Search</label>
+                <label for="sidebar-search" class="sr-only">{{ __('quickpanel.search') }}</label>
                 <div class="relative">
                     <div
                         class="flex absolute inset-y-0 start-0 items-center ps-3 pointer-events-none"
@@ -95,8 +95,8 @@
                         type="text"
                         name="search"
                         id="sidebar-search"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                        placeholder="Search"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full ps-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                        placeholder="{{ __('quickpanel.search') }}"
                     />
                 </div>
             </form>
