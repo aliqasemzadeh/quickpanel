5d9b3e1e5f0f1738b3e310ccf4144fc574fbcc67
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-09-12 07:13:41 +0330

    Add SetUserAdminCommand to assign administrator role via console command

---
+++ app/Console/Commands/System/Administrator/SetUserAdminCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands\System\Administrator;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class SetUserAdminCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'system:administrator:set-user-admin-command';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $userId = $this->ask('UserId');
+        try {
+            $user = User::findOrFail($userId);
+            $user->assignRole('user');
+            $user->assignRole('administrator');
+            $this->info('User Set as Administrator');
+        } catch (\Exception $e) {
+            $this->error($e->getMessage());
+        }
+
+    }
+}
