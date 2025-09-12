bf6b7117e90728941e6b79aa7605177d6baf4d93
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-09-12 08:05:09 +0330

    Use translation strings for search label and placeholder in quick panel.

--- resources/views/layouts/global/search.blade.php
+++ resources/views/layouts/global/search.blade.php
@@ -1,6 +1,6 @@
 <form action="#" method="GET" class="hidden md:block md:ps-2">
-    <label for="topbar-search" class="sr-only">Search</label>
-    <div class="relative md:w-64 md:w-96">
+    <label for="topbar-search" class="sr-only">{{ __('quickpanel.search') }}</label>
+    <div class="relative md:w-96">
         <div
             class="flex absolute inset-y-0 start-0 items-center ps-3 pointer-events-none"
         >
@@ -21,8 +21,8 @@
             type="text"
             name="email"
             id="topbar-search"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-            placeholder="Search"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+            placeholder="{{ __('quickpanel.search') }}"
         />
     </div>
 </form>
