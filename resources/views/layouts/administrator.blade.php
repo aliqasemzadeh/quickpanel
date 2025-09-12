ff3178ee381ca6183549eedcdbb0bedbee6fe049
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-09-12 07:31:35 +0330

    Add Farsi translations for permissions, roles, and quick panel strings; update layouts for RTL support

--- resources/views/layouts/administrator.blade.php
+++ resources/views/layouts/administrator.blade.php
@@ -17,6 +17,9 @@
                     data-drawer-target="drawer-navigation"
                     data-drawer-toggle="drawer-navigation"
                     aria-controls="drawer-navigation"
+                    @if(__('quickpanel.direction') == 'rtl')
+                        data-drawer-placement="right"
+                    @endif
                     class="p-2 mr-2 text-gray-600 rounded-lg cursor-pointer md:hidden hover:text-gray-900 hover:bg-gray-100 focus:bg-gray-100 dark:focus:bg-gray-700 focus:ring-2 focus:ring-gray-100 dark:focus:ring-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
                 >
                     <svg
