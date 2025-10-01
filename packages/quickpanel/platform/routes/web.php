cd71676d5179c4e8432abe8ec59a59a3feda5be8
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-10-01 19:26:45 +0330

    Wrap web routes in 'web' middleware group across all route files and reintroduce CSRF token meta tag in layout.

--- packages/quickpanel/platform/routes/web.php
+++ packages/quickpanel/platform/routes/web.php
@@ -1,10 +1,12 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', QuickPanel\Platform\Livewire\Front\Home\Index::class)->name('home');
+Route::middleware('web')->group(function () {
+    Route::get('/', QuickPanel\Platform\Livewire\Front\Home\Index::class)->name('home');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/user/dashboard/index', QuickPanel\Platform\Livewire\User\Dashboard\Index::class)->name('user.dashboard.index');
-    Route::get('/user/setting/profile/index', QuickPanel\Platform\Livewire\User\Setting\Profile\Index::class)->name('user.setting.profile.index');
-    Route::get('/user/setting/password/index', QuickPanel\Platform\Livewire\User\Setting\Password\Index::class)->name('user.setting.password.index');
+    Route::middleware('auth')->group(function () {
+        Route::get('/user/dashboard/index', QuickPanel\Platform\Livewire\User\Dashboard\Index::class)->name('user.dashboard.index');
+        Route::get('/user/setting/profile/index', QuickPanel\Platform\Livewire\User\Setting\Profile\Index::class)->name('user.setting.profile.index');
+        Route::get('/user/setting/password/index', QuickPanel\Platform\Livewire\User\Setting\Password\Index::class)->name('user.setting.password.index');
+    });
 });
