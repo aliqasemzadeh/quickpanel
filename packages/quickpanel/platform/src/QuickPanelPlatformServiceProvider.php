8bc7c1d5df65d5cf43c92ed2ea342d7b2bbd3ce6
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-10-01 10:23:28 +0330

    Add authentication routes and loading logic for `Platform` package

--- packages/quickpanel/platform/src/QuickPanelPlatformServiceProvider.php
+++ packages/quickpanel/platform/src/QuickPanelPlatformServiceProvider.php
@@ -16,6 +16,7 @@ class QuickPanelPlatformServiceProvider extends ServiceProvider
     {
         // Routes
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadRoutesFrom(__DIR__.'/../routes/auth.php');
 
         // Views
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'platform');
