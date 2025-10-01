8c4e37de88b96d968054ed38df22721ddcfcb7cc
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-10-01 21:12:03 +0330

    Register Livewire components explicitly in QuickPanel Platform Service Provider; remove dynamic registration method.

--- packages/quickpanel/platform/src/QuickPanelPlatformServiceProvider.php
+++ packages/quickpanel/platform/src/QuickPanelPlatformServiceProvider.php
@@ -25,8 +25,47 @@ class QuickPanelPlatformServiceProvider extends ServiceProvider
         // Translations
         $this->loadTranslationsFrom(__DIR__.'/../lang', 'platform');
 
-        // Register all Livewire components in the package
-        $this->registerLivewireComponents();
+        // Register Livewire components
+        Livewire::component('platform.administrator.auth.change-password', \QuickPanel\Platform\Livewire\Administrator\Auth\ChangePassword::class);
+        Livewire::component('platform.administrator.auth.forget-password', \QuickPanel\Platform\Livewire\Administrator\Auth\ForgetPassword::class);
+        Livewire::component('platform.administrator.auth.login', \QuickPanel\Platform\Livewire\Administrator\Auth\Login::class);
+        Livewire::component('platform.administrator.auth.logout', \QuickPanel\Platform\Livewire\Administrator\Auth\Logout::class);
+        Livewire::component('platform.administrator.dashboard.index', \QuickPanel\Platform\Livewire\Administrator\Dashboard\Index::class);
+        Livewire::component('platform.administrator.setting-management.function.index', \QuickPanel\Platform\Livewire\Administrator\SettingManagement\Function\Index::class);
+        Livewire::component('platform.administrator.setting-management.option.index', \QuickPanel\Platform\Livewire\Administrator\SettingManagement\Option\Index::class);
+        Livewire::component('platform.administrator.user-management.admin.create', \QuickPanel\Platform\Livewire\Administrator\UserManagement\Admin\Create::class);
+        Livewire::component('platform.administrator.user-management.admin.edit', \QuickPanel\Platform\Livewire\Administrator\UserManagement\Admin\Edit::class);
+        Livewire::component('platform.administrator.user-management.admin.index', \QuickPanel\Platform\Livewire\Administrator\UserManagement\Admin\Index::class);
+        Livewire::component('platform.administrator.user-management.admin.permissions', \QuickPanel\Platform\Livewire\Administrator\UserManagement\Admin\Permissions::class);
+        Livewire::component('platform.administrator.user-management.admin.roles', \QuickPanel\Platform\Livewire\Administrator\UserManagement\Admin\Roles::class);
+        Livewire::component('platform.administrator.user-management.admin.table', \QuickPanel\Platform\Livewire\Administrator\UserManagement\Admin\Table::class);
+        Livewire::component('platform.administrator.user-management.permission.create', \QuickPanel\Platform\Livewire\Administrator\UserManagement\Permission\Create::class);
+        Livewire::component('platform.administrator.user-management.permission.edit', \QuickPanel\Platform\Livewire\Administrator\UserManagement\Permission\Edit::class);
+        Livewire::component('platform.administrator.user-management.permission.index', \QuickPanel\Platform\Livewire\Administrator\UserManagement\Permission\Index::class);
+        Livewire::component('platform.administrator.user-management.permission.table', \QuickPanel\Platform\Livewire\Administrator\UserManagement\Permission\Table::class);
+        Livewire::component('platform.administrator.user-management.role.create', \QuickPanel\Platform\Livewire\Administrator\UserManagement\Role\Create::class);
+        Livewire::component('platform.administrator.user-management.role.edit', \QuickPanel\Platform\Livewire\Administrator\UserManagement\Role\Edit::class);
+        Livewire::component('platform.administrator.user-management.role.index', \QuickPanel\Platform\Livewire\Administrator\UserManagement\Role\Index::class);
+        Livewire::component('platform.administrator.user-management.role.permissions', \QuickPanel\Platform\Livewire\Administrator\UserManagement\Role\Permissions::class);
+        Livewire::component('platform.administrator.user-management.role.table', \QuickPanel\Platform\Livewire\Administrator\UserManagement\Role\Table::class);
+        Livewire::component('platform.administrator.user-management.role.users', \QuickPanel\Platform\Livewire\Administrator\UserManagement\Role\Users::class);
+        Livewire::component('platform.administrator.user-management.user.create', \QuickPanel\Platform\Livewire\Administrator\UserManagement\User\Create::class);
+        Livewire::component('platform.administrator.user-management.user.edit', \QuickPanel\Platform\Livewire\Administrator\UserManagement\User\Edit::class);
+        Livewire::component('platform.administrator.user-management.user.index', \QuickPanel\Platform\Livewire\Administrator\UserManagement\User\Index::class);
+        Livewire::component('platform.administrator.user-management.user.permissions', \QuickPanel\Platform\Livewire\Administrator\UserManagement\User\Permissions::class);
+        Livewire::component('platform.administrator.user-management.user.roles', \QuickPanel\Platform\Livewire\Administrator\UserManagement\User\Roles::class);
+        Livewire::component('platform.administrator.user-management.user.table', \QuickPanel\Platform\Livewire\Administrator\UserManagement\User\Table::class);
+        Livewire::component('platform.auth.change-password', \QuickPanel\Platform\Livewire\Auth\ChangePassword::class);
+        Livewire::component('platform.auth.forget-password', \QuickPanel\Platform\Livewire\Auth\ForgetPassword::class);
+        Livewire::component('platform.auth.login', \QuickPanel\Platform\Livewire\Auth\Login::class);
+        Livewire::component('platform.auth.logout', \QuickPanel\Platform\Livewire\Auth\Logout::class);
+        Livewire::component('platform.auth.register', \QuickPanel\Platform\Livewire\Auth\Register::class);
+        Livewire::component('platform.auth.verify-email', \QuickPanel\Platform\Livewire\Auth\VerifyEmail::class);
+        Livewire::component('platform.front.faq.index', \QuickPanel\Platform\Livewire\Front\Faq\Index::class);
+        Livewire::component('platform.front.home.index', \QuickPanel\Platform\Livewire\Front\Home\Index::class);
+        Livewire::component('platform.user.dashboard.index', \QuickPanel\Platform\Livewire\User\Dashboard\Index::class);
+        Livewire::component('platform.user.setting.password.index', \QuickPanel\Platform\Livewire\User\Setting\Password\Index::class);
+        Livewire::component('platform.user.setting.profile.index', \QuickPanel\Platform\Livewire\User\Setting\Profile\Index::class);
 
         // Register console commands
         if ($this->app->runningInConsole()) {
@@ -58,48 +97,5 @@ class QuickPanelPlatformServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/../lang' => resource_path('lang/vendor/platform'),
         ], 'platform-lang');
-    }
-
-    private function registerLivewireComponents(): void
-    {
-        $baseNamespace = 'QuickPanel\\Platform\\Livewire';
-        $baseDir = __DIR__ . DIRECTORY_SEPARATOR . 'Livewire';
-
-        if (!is_dir($baseDir)) {
-            return;
-        }
-
-        $iterator = new \RecursiveIteratorIterator(
-            new \RecursiveDirectoryIterator($baseDir, \FilesystemIterator::SKIP_DOTS)
-        );
-
-        foreach ($iterator as $file) {
-            /** @var \SplFileInfo $file */
-            if ($file->getExtension() !== 'php') {
-                continue;
-            }
-
-            $relativePath = str_replace($baseDir . DIRECTORY_SEPARATOR, '', $file->getPathname());
-            $class = $baseNamespace . '\\' . str_replace([DIRECTORY_SEPARATOR, '.php'], ['\\', ''], $relativePath);
-
-            if (!class_exists($class)) {
-                continue;
-            }
-
-            if (!is_subclass_of($class, \Livewire\Component::class)) {
-                continue;
-            }
-
-            $namePath = str_replace('.php', '', $relativePath);
-            $normalized = str_replace(DIRECTORY_SEPARATOR, '/', $namePath);
-            $segments = array_map('strtolower', explode('/', $normalized));
-            $alias = 'quick-panel.platform.livewire.' . implode('.', $segments);
-            $shortAlias = implode('.', $segments);
-
-            \Livewire\Livewire::component($alias, $class);
-            // Also register a short alias (without the package prefix) to match Blade usages like
-            // <livewire:administrator.user-management.admin.table />
-            \Livewire\Livewire::component($shortAlias, $class);
-        }
     }
 }
