1a6e50035be203004a1e007c11516e0d87b2c9b3
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-10-02 08:38:51 +0330

    Update `OAuthWelcomeMail` references to use the `platform` namespace in Google and GitHub controllers

--- packages/quickpanel/platform/src/Http/Controllers/Auth/GithubController.php
+++ packages/quickpanel/platform/src/Http/Controllers/Auth/GithubController.php
@@ -3,7 +3,7 @@
 namespace QuickPanel\Platform\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Mail\OAuthWelcomeMail;
+use QuickPanel\Platform\Mail\OAuthWelcomeMail;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
