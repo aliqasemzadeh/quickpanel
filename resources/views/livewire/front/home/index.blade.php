707d5043e01cd867b053275d55ba320a7d2b63ac
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-10-12 09:26:30 +0330

    Add Livewire front home component and route for homepage rendering

---
+++ resources/views/livewire/front/home/index.blade.php
@@ -0,0 +1,6 @@
+<div>
+@php
+$color = 'yellow';
+@endphp
+    <x-fb.button type="button" :color="$color" wire:click="test">Test</x-fb.button>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', \App\Livewire\Front\Home\Index::class)->name('home');
 
 require_once __DIR__.'/auth.php';
