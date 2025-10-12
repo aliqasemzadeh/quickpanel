5b9a6ae33c097b5f85df889e02221b2e43883da6
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-10-12 10:10:53 +0330

    Remove unused code and add dynamic page title for the homepage component

--- resources/views/livewire/front/home/index.blade.php
+++ resources/views/livewire/front/home/index.blade.php
@@ -1,9 +1,5 @@
+<x-slot name="title">
+    {{ __('platform::common.home') }}
+</x-slot>
 <div>
-@php
-$color = 'yellow';
-@endphp
-    <x-fb.button type="button" :color="$color" wire:click="test">Test</x-fb.button>
-    <x-fb.alert type="success" dismissible>
-1444
-    </x-fb.alert>
 </div>
